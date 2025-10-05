9115b578d24835cc799cca38365a52dca53ebd99
Nikita <nick.gusarov25@yandex.ru>
2025-10-05 12:27:38 +0300

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,13 @@ import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-
-export default defineConfig(() => {
-
-  return {
-    plugins: [react(), tsconfigPaths()],
-    publicDir: './public',
-    server: {
-      host: true,
-      open: true,
-      port: 7000,
-      hmr: true,
-    },
-    envPrefix: ['VITE_'],
-    css: {
-      preprocessorOptions: {
-        scss: {
-          api: 'modern',
-        },
-      },
-    },
-  };
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  publicDir: './public',
+  server: {
+    host: true,
+    open: true,
+    port: 7000,
+    hmr: true,
+  },
 });
